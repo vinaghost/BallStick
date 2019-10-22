07cc0ed321e9ebb4ccef3d7c53938ba4d6fd6f10
QUANG NGUYỄN VINH <18120229@student.hcmus.edu.vn>
2019-10-22 09:48:55 +0700

    cmt Stick.cpp

--- Stick.cpp
+++ Stick.cpp
@@ -1,11 +1,13 @@
 ﻿#include "Stick.h"
 #include "Graphic.h"
 
-
-
-Stick::Stick(int left, int top, int width, int height, int size) : left(left), top(top), height(height), width(width), size(size) {
+// Hàm khởi tạo điểm đầu của Stick và các giá trị về kích thước Stick, giới hạn di chuyển của Stick(giới hạn bàn chơi).
+Stick::Stick(int left, int top, int width, int height, int size) : left(left), top(top), height(height), width(width), size(size) 
+{
+	//Xét sự tồn tại của Stick.
 	this->spawned = false;
-
+	
+	//Khởi tạo tọa độ điểm đầu của Stick(x,y).
 	x = left;
 	y = top;
 }
@@ -13,11 +15,13 @@ Stick::Stick(int left, int top, int width, int height, int size) : left(left), t
 Stick::~Stick() {}
 
 void Stick::setX(int x) {
+	//Xét điều kiện: tọa độ x được nhập vào phải nằm trong chiều rộng của bàn và độ dài Stick lúc này không vượt qua chiều dài của bàn.
 	if (x > this->left && x + this->size < this->left + this->width) 
 		this->x = x;
 }
 
 void Stick::setY(int y) {
+	//Xét điều kiện: tọa độ y được nhập vào phải nằm trong chiều dài của bàn.
 	if (y > this->top && y < this->top + this->height)
 		this->y = y;
 }
@@ -40,19 +44,19 @@ int Stick::getsize() {
 }
 
 void Stick::spawn() {
-	// đã xuất hiện trên màn hình
+	// đã xuất hiện trên màn hình.
 	if (this->spawned) return;
 
-
+	// Ngược lại, vẽ Stick và xác nhận Stick đã xuất hiện.
 	Graphic::drawStick(*this);
 	this->spawned = true;
 }
 
 void Stick::despawn() {
-	// đã biến mất khỏi màn hình
+	// đã biến mất khỏi màn hình.
 	if (!this->spawned) return;
 
-
+	// Ngược lại, xóa Stick và xác nhận Stick đã biến mất.
 	Graphic::deleteStick(*this);
 	this->spawned = false;
 }
